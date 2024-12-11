b736c93c982e68ddbb8d1f9cec2b3e0d05ce5747
BrandonFern <fernandezbrand081@gmail.com>
2024-12-11 10:30:59 -0600

    IMPORTANT. Create user data on registration

--- user.js
+++ user.js
@@ -15,9 +15,11 @@ const firebaseConfig = {
   auth.onAuthStateChanged((user) => {
     if (user) {
       const userId = user.uid;
+  
       const isGoogleSignIn = user.providerData.some(
         (provider) => provider.providerId === 'google.com'
       );
+  
       const userRef = db.collection("Players").doc(userId);
   
       userRef.get().then((doc) => {
@@ -31,6 +33,53 @@ const firebaseConfig = {
           populateAchievements(userData.achievement);
           populateStats(userData.stats);
           populateLoot(userData.equipment);
+        } else {
+          const newUserName = isGoogleSignIn ? user.displayName : user.email;
+          userRef.set({
+            name: newUserName,
+            email: user.email,
+            stats: {
+              kills: 0,
+              deaths: 0,
+              lootamount: 0,
+            },
+            level: 1,
+            equipment: [],
+            achievement: {
+              "Kill 5 enemies": false,
+              "Kill 10 enemies": false,
+              "Kill 25 enemies": false,
+              "Collect 5 items": false,
+              "Collect 10 items": false,
+              "Collect 25 items": false,
+              "Walk": false,
+              "Melee": false,
+              "Jump": false,
+              "Sprint": false,
+            },
+          }).then(() => {
+            console.log("New user document created!");
+            document.getElementById('user-name').innerText = `${newUserName} (Level 1)`;
+            populateAchievements({
+              "Kill 5 enemies": false,
+              "Kill 10 enemies": false,
+              "Kill 25 enemies": false,
+              "Collect 5 items": false,
+              "Collect 10 items": false,
+              "Collect 25 items": false,
+              "Walk": false,
+              "Melee": false,
+              "Jump": false,
+              "Sprint": false,
+            });
+            populateStats({
+              kills: 0,
+              deaths: 0,
+              lootamount: 0,
+            });
+          }).catch((error) => {
+            console.error("Error creating user document:", error);
+          });
         }
       }).catch((error) => {
         console.log("Error getting user document:", error);
